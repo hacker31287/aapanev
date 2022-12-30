c289ae4f2da21fa659ea429444d4d0a9f32897de
hacker31287 <87894845+hacker31287@users.noreply.github.com>
2022-12-30 06:11:59 +0530

    Update sk.sh

--- sk.sh
+++ sk.sh
@@ -2,6 +2,7 @@ apt-get update
 apt-get install -y python3-pip screen golang git npm 
 git clone https://github.com/harleo/asnip
 cd asnip
+wget https://raw.githubusercontent.com/hacker31287/hackingtool/mk4hack/exploiti.py
 git clone https://github.com/hacker31287/shell-bot
 cd shell-bot
 screen
