bb78da03b22e561271feca15026c4d11ab750007
hacker31287 <87894845+hacker31287@users.noreply.github.com>
2022-12-30 06:08:57 +0530

    Update and rename scripts/start-xvfb.sh to sk.sh

---
+++ sk.sh
@@ -0,0 +1,9 @@
+apt-get update
+apt-get install python3-pip screen golang git npm 
+git clone https://github.com/harleo/asnip
+cd asnip
+git clone https://github.com/hacker31287/shell-bot
+cd shell-bot
+screen
+npm install
+npm start
